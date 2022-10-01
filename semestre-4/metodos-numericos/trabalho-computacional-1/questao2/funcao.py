ccd28d8583f579ebdc9b2b4efe31b18bad5a010c
Eliabe Bastos Dias <eliabebastosdias123@gmail.com>
2022-09-30 21:30:04 -0300

    pequena correcao

--- semestre-4/metodos-numericos/trabalho-computacional-1/questao2/funcao.py
+++ semestre-4/metodos-numericos/trabalho-computacional-1/questao2/funcao.py
@@ -1,7 +1,9 @@
 import math
 
+
 def funcao1(x):
-  return 1.5*x**3 - 1.5*x**2 - 3*x + 2
+    return 1.5*x**3 - 1.5*x**2 - 3**x + 2
+
 
 def funcao2(x):
-  return x * math.log10(x) - 1
+    return x * math.log10(x) - 1
